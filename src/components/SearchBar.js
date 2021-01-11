c2cf543bd59b8c4ffbda4b2d9c3f82b096872efa
JoshSevy <joshuasevy@outlook.com>
2021-01-10 17:58:20 -0700

    Add onchange and styling to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-
-
+  state = { term: '' }
   render() {
-    return <div>SearchBar</div>
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={(e) => e.preventDefault()}>
+          <div className="field">
+            <label htmlFor="search">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              id="search"
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    )
   }
 }
 
