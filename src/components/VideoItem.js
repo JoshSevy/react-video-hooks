bdbfac0edd6c9763b8d805abca15782476b42bbd
JoshSevy <joshuasevy@outlook.com>
2021-01-10 19:05:05 -0700

    Start styling video items

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const VideoItem = ({ video }) => {
-  return <div>VideoItem</div>
+  return (
+    <div className="item" key={video.id.videoId}>
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.description}
+      />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  )
 }
 
 export default VideoItem;
