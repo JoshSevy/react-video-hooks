b6ad1911c6ab63b37e480027c0bd16f297b287e4
JoshSevy <joshuasevy@outlook.com>
2021-01-10 18:06:20 -0700

    Add event hanglers to searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  state = { term: '' }
+  state = { term: '' };
+
+  onHandleChange = (e) => {
+    this.setState({ term: e.target.value });
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  //TODO: Make sure we call callback from parent component
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form" onSubmit={(e) => e.preventDefault()}>
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="search">Video Search</label>
             <input
               type="text"
               value={this.state.term}
               id="search"
-              onChange={(e) => this.setState({ term: e.target.value })}
+              onChange={this.onHandleChange}
             />
           </div>
         </form>
